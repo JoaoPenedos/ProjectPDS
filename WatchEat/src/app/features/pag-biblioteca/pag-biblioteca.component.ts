bbf939967732d15b5f676ef4ab57fc0a6b103c6f
JoaoMeira-23505 <95706191+JoaoMeira-23505@users.noreply.github.com>
2023-05-21 00:15:21 +0100

    Meira pagina biblioteca

--- WatchEat/src/app/features/pag-biblioteca/pag-biblioteca.component.ts
+++ WatchEat/src/app/features/pag-biblioteca/pag-biblioteca.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ConteudosDataService} from "../../_shared/services/Conteudos/conteudos-data.service";
 
 @Component({
   selector: 'app-biblioteca',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./pag-biblioteca.component.css']
 })
 export class PagBibliotecaComponent {
+  conteudos: any[] = [];
 
+  constructor(private conteudoDataService: ConteudosDataService) {}
+
+  ngOnInit() {
+    this.conteudoDataService.getConteudos().subscribe((data: Object) => {
+      this.conteudos = data as any[]; // Cast the data to an array type
+    });
+  }
 }
