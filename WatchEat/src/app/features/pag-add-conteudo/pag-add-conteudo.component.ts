a39018eeba72f14c064055f6f9ac7239d4585ff4
manuelfernandes02 <a23502@alunos.ipca.pt>
2023-05-26 11:55:53 +0100

    Manuel, editar pag amigos e adicionar conteudo

--- WatchEat/src/app/features/pag-add-conteudo/pag-add-conteudo.component.ts
+++ WatchEat/src/app/features/pag-add-conteudo/pag-add-conteudo.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pag-add-conteudo',
   templateUrl: './pag-add-conteudo.component.html',
   styleUrls: ['./pag-add-conteudo.component.css']
 })
-export class PagAddConteudoComponent {
+export class PagAddConteudoComponent implements OnInit {
+  conteudoForm!: FormGroup;
+  submitted = false;
 
+  tipoconteudo!: string;
+  duracao!: number;
+  conteudoid!: number;
+  ntemporadas!: number;
+  nepisodios!: number;
+  estado!: string;
+  datafim!: Date;
+  conteudoidserie!: number;
+  titulo!: string;
+  direcao!: string;
+  rating!: number;
+  sinopse!: string;
+  trailer!: string;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+    this.conteudoForm = this.formBuilder.group({
+      tipoconteudo: ['', Validators.required],
+      duracao: ['', Validators.required],
+      conteudoid: ['', Validators.required],
+      ntemporadas: ['', Validators.required],
+      nepisodios: ['', Validators.required],
+      estado: ['', Validators.required],
+      datafim: ['', Validators.required],
+      conteudoidserie: ['', Validators.required],
+      titulo: ['', Validators.required],
+      direcao: ['', Validators.required],
+      rating: ['', Validators.required],
+      sinopse: ['', Validators.required],
+      trailer: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.conteudoForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.conteudoForm.invalid) {
+      return;
+    }
+
+    // Handle form submission logic here
+  }
 }
