312f15062c09a0fa057d1d452e57a677e58df78a
bifes <bifesjunior97@gmail.com>
2023-05-31 17:14:37 +0100

    Pag Inicial -Bugaça

--- WatchEat/src/app/features/pag-produtos/pag-produtos.component.ts
+++ WatchEat/src/app/features/pag-produtos/pag-produtos.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {PagamentosDataService} from "../../_shared/services/Pagamentos/pagamentos-data.service";
 import {ProdutosDataService} from "../../_shared/services/Produtos/produtos-data.service";
 import decode from "jwt-decode";
+import {formatNumber} from "@angular/common";
 
 @Component({
   selector: 'app-pagina-produtos',
@@ -20,4 +21,27 @@ export class PagProdutosComponent {
       this.produtos = data as any[];
     });
   }
+  adicionarProduto(prodId: string): void {
+    const produtosQuantidades: { [key: string]: number } = {};
+    // Verifica se o produto já existe na variável local
+    if (prodId in produtosQuantidades) {
+      // Se o produto já existir, incrementa a quantidade em 1
+      produtosQuantidades[prodId]++;
+    } else {
+      // Se o produto não existir, adiciona com quantidade 1
+      produtosQuantidades[prodId] = 1;
+    }
+    const quantidadeTotal = Object.values(produtosQuantidades).reduce((acc, curr) => acc + curr, 0);
+    const quantidadeElement = document.getElementById("quantidade-produtos");
+    if (quantidadeElement) {
+      quantidadeElement.textContent = quantidadeTotal.toString();
+    }
+    // Exibe a quantidade no console (apenas para demonstração)
+    console.log(produtosQuantidades[prodId]);
+  }
+
 }
+
+// Função para guardar a quantidade de produtos
+
+
